a5ab213b46fe83693645cef51b97e0a7f84f2d80
Artem <artem@rem.sh>
2026-07-16 05:56:08 +0300

    Use `path::string()` instead of `path::c_str()` for portability (#3079)
    So far, several places passed `std::filesystem::path::c_str()` to functions that expect a `std::string` or `const char*`. On Windows, the native character type of a path is `wchar_t`, so `path::c_str()` returns `const wchar_t*` (and the implicit conversion from `path` to a string yields `std::wstring`), so these calls do not compile. Now these places use `path::string()`, which is portable.

--- test/DeltaTriplesTest.cpp
+++ test/DeltaTriplesTest.cpp
@@ -745,8 +745,8 @@ TEST_F(DeltaTriplesTest, storeAndRestoreFromEmptySet) {
   auto tmpFile =
       ql::filesystem::temp_directory_path() / "testEmptyDeltaTriples";
   // Make sure no artifacts from previous crashed runs exists.
-  ql::filesystem::remove(tmpFile);
-  absl::Cleanup cleanup{[&tmpFile]() { ql::filesystem::remove(tmpFile); }};
+  std::filesystem::remove(tmpFile);
+  absl::Cleanup cleanup{[&tmpFile]() { std::filesystem::remove(tmpFile); }};
   deltaTriples.setPersists(tmpFile.string());
   // Write "empty" file
   EXPECT_NO_THROW(deltaTriples.writeToDisk());
@@ -850,6 +850,7 @@ TEST_F(DeltaTriplesTest, storeAndRestoreData) {
   {
     DeltaTriples deltaTriples{testQec->getIndex()};
     deltaTriples.setPersists(tmpFile.string());
+    deltaTriples.setPersists(tmpFile.string());
     deltaTriples.readFromDisk();
 
     auto cancellationHandle =
@@ -871,6 +872,7 @@ TEST_F(DeltaTriplesTest, storeAndRestoreData) {
   }
   {
     DeltaTriples deltaTriples{testQec->getIndex()};
+    deltaTriples.setPersists(tmpFile.string());
     deltaTriples.setPersists(tmpFile.string());
     deltaTriples.readFromDisk();
 
